6f37018f8b7f4be2a6995e13ec5bf90e592971b7
mucahitcetin <mucahitcetin01@gmail.com>
2025-01-02 16:01:02 +0300

    env fixed and project finished

--- api/yelp.js
+++ api/yelp.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { YELP_API_KEY } from '@env';
 
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses/',
   headers: {
-    Authorization: `Bearer ${YELP_API_KEY}`,
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_YELP_API_KEY}`,
   },
 });
